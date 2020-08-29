5c5ef815202fa635ea39c0fa356fbd8f5752dc51
edgonzalez-9 <edgonzalezhernandez9@gmail.com>
2020-08-28 21:52:37 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
+
+const saveInterval = document.getElementById( 'interval' )
+const startSaving = document.getElementById( 'save_lincoln' ).addEventListener( "click", () => ( saveLincoln( parseInt( saveInterval.value ) ) ) )
+const zombieFace = document.getElementById( 'foreground' )
+const hiddenMessage = document.getElementById( 'hidden-message' )
+
+function saveLincoln( interval ) {
+  if ( isNaN( interval ) ) {
+    interval = 100
   }
-  $('#foreground').fadeOut(interval);
+  
 }
